bc6cded1cc53684d1fbfc7912815c9765d6713ba
Joshua M Singh <joshuasingh522@gmail.com>
2020-02-22 18:15:05 +0530

    prodchanges1

---
+++ build/precache-manifest.3f63670a848d1f1ffdacfbdaf155254c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231306ef87613f8ff45e4e47b48a6049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7fbd9b862aee784da64",
+    "url": "/static/css/2.6cfe5639.chunk.css"
+  },
+  {
+    "revision": "fb5652ec3ed4b8a70eb8",
+    "url": "/static/css/main.a684cb28.chunk.css"
+  },
+  {
+    "revision": "a7fbd9b862aee784da64",
+    "url": "/static/js/2.74eaf7ff.chunk.js"
+  },
+  {
+    "revision": "fb5652ec3ed4b8a70eb8",
+    "url": "/static/js/main.b102a2f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
